50f393b04a886102356088c51959b6901f6fb31b
Luis Corvalan <lrcorvalan@gmail.com>
2022-01-02 13:32:14 -0600

    Initial Commit

---
+++ Needle/NeedleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeedleApp.swift
+//  Needle
+//
+//  Created by Luis Corvalan on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct NeedleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
